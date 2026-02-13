2ebde15f3a202464c7eccd8c2e1dd4967dda3c72
roidaradal <johnroy.daradal@goodkredit.com>
2026-02-13 09:01:41 +0800

    v0.3.15 - Add middleware to RegisterRoutes

--- web/handlers.go
+++ web/handlers.go
@@ -10,14 +10,17 @@ type Handler struct {
 }
 
 // Register web handlers, returns the number of endpoints
-func RegisterRoutes(server *gin.Engine, baseURL string, handlers []Handler) (*gin.RouterGroup, int) {
+func RegisterRoutes(server *gin.Engine, baseURL string, handlers []Handler, middlewares ...gin.HandlerFunc) int {
 	count := 0
 	router := server.Group(baseURL)
+	if len(middlewares) > 0 {
+		router.Use(middlewares...)
+	}
 	for _, handler := range handlers {
 		for endpoint, handlerFn := range handler.Map {
 			router.Handle(handler.Verb, endpoint, handlerFn)
 			count += 1
 		}
 	}
-	return router, count
+	return count
 }
